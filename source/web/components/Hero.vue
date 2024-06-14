852821fc32938f4021c9377525790b21d3f5a97d
sharukhi <sharukhi@sharukhi.xyz>
2024-06-14 14:31:48 +0600

    update changelog page

--- source/web/components/Hero.vue
+++ source/web/components/Hero.vue
@@ -2,7 +2,8 @@
   <header class="py-12 px-4 sm:px-6 lg:px-8">
     <div class="relative mx-auto max-w-[37.5rem] pt-20 text-center pb-20">
       <div class="flex items-center justify-center space-x-3">
-        <img loading="lazy" src="/icon.png" alt="Logspot" class="h-20 w-20 rounded-full border-2 border-slate-600 border-opacity-20" />
+        <img loading="lazy" src="/icon.png" alt="Logspot"
+          class="h-20 w-20 rounded-full border-2 border-slate-600 border-opacity-20" />
         <h1 class="text-4xl font-extrabold tracking-tight text-slate-700 sm:text-5xl">
           Lofi-Box
         </h1>
@@ -10,18 +11,19 @@
       <p class="mt-4 text-lg leading-7 text-slate-600">
         ✨ Relaxing lo-fi tunes directly within your web browser.
       </p>
-      <div class="grid grid-cols-1 gap-0 sm:grid-cols-1 lg:grid-cols-2 md:grid-cols-2 lg:mx-[115px] md:mx-[115px] sm:mx-[115px] mx-0 place-items-center my-5">
-      <div class="mb-2">
-        <a href="https://chromewebstore.google.com/detail/lofi-box/fbogegnhfhlbdjjhompihcgcenhdclhj">
-          <img class="h-12"src="/chrome.svg">
-        </a>
-      </div> 
-      <div class="mb-2">
-        <a href="https://addons.mozilla.org/en-US/firefox/addon/lofi-box/">
-          <img class="h-12" src="/firefox.svg">
-        </a>
+      <div
+        class="grid grid-cols-1 gap-0 sm:grid-cols-1 lg:grid-cols-2 md:grid-cols-2 lg:mx-[115px] md:mx-[115px] sm:mx-[115px] mx-0 place-items-center my-5">
+        <div class="mb-2">
+          <a href="https://chromewebstore.google.com/detail/lofi-box/fbogegnhfhlbdjjhompihcgcenhdclhj">
+            <img class="h-12" src="/svg/chrome_download_dark.svg">
+          </a>
+        </div>
+        <div class="mb-2">
+          <a href="https://addons.mozilla.org/en-US/firefox/addon/lofi-box/">
+            <img class="h-12" src="/svg/firefox_download_dark.svg">
+          </a>
+        </div>
       </div>
-    </div>
     </div>
   </header>
 </template>
