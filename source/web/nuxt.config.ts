b05daf1aa64b2f2b1065ca9e9a8936151069edee
Sharukhi <sharukhi@sharukhi.xyz>
2024-11-28 17:15:05 +0600

    Minor fixes
    Fixed viewport problem and removed web source

--- source/web/nuxt.config.ts
+++
@@ -1,9 +0,0 @@
-export default ({
-    modules: ['@nuxtjs/tailwindcss', '@nuxt/content', 'nuxt-icon'],
-    content: {
-        highlight: {
-            theme: 'nord',
-            preload: ['js', 'css', 'html', 'md', 'ts', 'tsx', 'vue', 'python', 'ruby', 'java'],
-        }
-    }
-})
